98023ffdb3aa54e730c352dc0f08516115b925f8
ThanhTrung6886 <lethanhtrung271106@gmail.com>
2026-09-21 14:19:48 +0700

    Update auth.js

--- auth.js
+++ auth.js
@@ -1,6 +1,6 @@
 function login(username, password) {
     // Update login for VLU Lab 01
-if (username === "admin" && password === "9999")
+if (username === "admin" && password === "123")
         return true;
     }
 
